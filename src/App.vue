cc4c85098ed2bbe835faea55d057a67c2fdeba1f
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-24 11:57:37 -0500

    adding component logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
 <h1>Ninja reaction Timer </h1>
-<button @click="start">play</button>
+<button @click="start" :disabled="isPlaying">play</button>
+<Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from "./components/Block.vue"
 
 export default {
   name: 'App',
   components: {
+    Block
   },
   data(){
     return {
@@ -19,7 +22,6 @@ export default {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-      console.log(this.delay)
     }
   }
 }
---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="block" v-if="showBlock">
+      click me  
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data(){
+        return {
+            showBlock: false
+        }
+    },
+    mounted(){
+        console.log('component mounted')
+        setTimeout(() => {
+            this.showBlock =true
+            console.log('delay: '+this.delay)
+            }, this.delay)
+    },
+    updated(){
+        console.log('component updated')
+    }
+}
+</script>
+
+<style>
+.block {
+    width:400px;
+    border-radius:20px;
+    background:#0faf87;
+    color:white;
+    text-align:center;
+    padding:100px 0;
+    margin:40px auto;
+}
+</style>
