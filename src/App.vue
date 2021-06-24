875de506582d1e6926f1ccb01a63fa79c8d42839
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-24 13:28:48 -0500

    adding result

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
 <h1>Ninja reaction Timer </h1>
 <button @click="start" :disabled="isPlaying">play</button>
-<Block v-if="isPlaying" :delay="delay"/>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<p v-if="showResults"> Reaction time: {{score}} ms</p>
 </template>
 
 <script>
@@ -15,15 +16,24 @@ export default {
   data(){
     return {
       isPlaying: false,
-      delay:null
+      delay:null,
+      score: null,
+      showResults: false
     }
   },
   methods:{
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
+
 }
 </script>
 
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
       click me  
     </div>
 </template>
@@ -9,18 +9,27 @@ export default {
     props: ['delay'],
     data(){
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted(){
-        console.log('component mounted')
         setTimeout(() => {
             this.showBlock =true
-            console.log('delay: '+this.delay)
+            this.startTimer()
             }, this.delay)
-    },
-    updated(){
-        console.log('component updated')
+        },
+    methods:{
+        startTimer(){
+            this.timer = setInterval(() =>{
+                this.reactionTime +=10
+            },10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
     }
 }
 </script>
